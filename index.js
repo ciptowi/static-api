bb67b4c43cc6ecbfb4d2d586d7ed1dd7d8f3cb43
ciptowi <ciptowidarto@gmail.com>
2021-12-10 22:01:14 +0700

    install express-ejs-layouts, created dir layouts & file main-layout.ejs

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const expressLayouts = require("express-ejs-layouts");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(expressLayouts);
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    title: "Home",
+    layout: "layouts/main-layout",
+  });
 });
 
 app.listen(port, () => console.log(`Program running port ${port}`));
